fe41e4bcb79f8a36ddce17e263c4a296f8ca197f
Mari Mohr <marianamohr@gmail.com>
2024-11-12 19:40:35 -0300

    aula 39

--- src/app.js
+++ src/app.js
@@ -8,7 +8,27 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUiExpress from 'swagger-ui-express';
 
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+import { dirname } from 'path';
+const __dirname = dirname(__filename);
+console.log("__dirname",`${__dirname}/docs/Users.yaml`)
+
+const swaggerOptions = {
+    definition:{
+        openapi: "3.0.0",
+        info:{
+            title:"API da AdoptMe",
+            description:"API for my application"
+        },
+        
+    },
+    apis:[`${__dirname}/docs/Users.yaml`]
+}
+const specs = swaggerJSDoc(swaggerOptions);
 
 dotenv.config();
 
@@ -25,5 +45,6 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
