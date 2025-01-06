c6fd8d68aab1991d3b8c604b8f18a29e4e540fde
Mari Mohr <marianamohr@gmail.com>
2025-01-06 20:07:51 -0300

    ajuste variaveis

--- src/app.js
+++ src/app.js
@@ -8,11 +8,27 @@ import petsRouter from "./routes/pets.router.js";
 import adoptionsRouter from "./routes/adoption.router.js";
 import sessionsRouter from "./routes/sessions.router.js";
 
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
+
 dotenv.config();
+console.log(`./docs/**/*.yaml`);
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API da AdoptMe",
+      description: "API for my application",
+    },
+  },
+  apis: [`./docs/**/*.yaml`],
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-mongoose.connect("mongodb+srv://marianamohr:c9zKW4F8Vadmdn5d@cluster0.zm7bida.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,5 +37,6 @@ app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
 app.use("/api/adoptions", adoptionsRouter);
 app.use("/api/sessions", sessionsRouter);
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
